300b0c9f3cce7967cc1d40d5620e46bc7cb50961
Alex <114047696+bigspaceworm@users.noreply.github.com>
2025-08-26 15:23:45 -0600

    Add button to change grid size

--- scripts.js
+++ scripts.js
@@ -1,38 +1,68 @@
-const GRID_SIZE = 16;
-const BLOCK_SIZE = 20;
-const sketchContainer = document.querySelector("#sketchContainer");
+const DEFAULT_GRID_SIZE = 16;
+const BLOCK_SIZE = 1.5; // Temp debug ?
+const GRID_SIDE = 26; const sketchContainer = document.querySelector("#sketchContainer");
+const buttonGridSize = document.querySelector(".gridSize").querySelector("button");
 
-function setGridSize(){
-	sketchContainer.style.height = (GRID_SIZE * BLOCK_SIZE) + "px";
-	sketchContainer.style.width = (GRID_SIZE* BLOCK_SIZE) + "px";
+function getUserGridSize(){
+	let userGridSize = Number(prompt("Write a number between 2-100 to set the size of the grid", 16));
+
+	while(userGridSize < 2 || userGridSize > 100){
+		userGridSize = Number(prompt("Invalid number, it MUST be between 2-100", ""));
+	}
+
+	return userGridSize;
 }
 
-function createGrid(){
-	for(let i = 0; i < GRID_SIZE *GRID_SIZE; i++){
+function setGridSize(){
+	sketchContainer.style.height = GRID_SIDE + "em";
+	sketchContainer.style.width = GRID_SIDE + "em";
+}
+
+function createGrid(gridBlockSize){
+	let blockSide = (GRID_SIDE / gridBlockSize) + "em";
+	for(let i = 0; i < gridBlockSize * gridBlockSize; i++){
 		const block = document.createElement("div");
 		block.classList.add("gridBlock");
-		block.style.height = BLOCK_SIZE + "px";
-		block.style.width = BLOCK_SIZE + "px";
+		block.style.height = blockSide;
+		block.style.width = blockSide;
 
 		sketchContainer.appendChild(block);
 	}
 }
 
+function deleteGrid(){
+	while(sketchContainer.lastChild){
+		sketchContainer.removeChild(sketchContainer.lastChild);
+	}
+}
+
+function changeBlockOnHover(){
+	const blocksHover = sketchContainer.querySelectorAll("div");
+
+	blocksHover.forEach((div) => {
+		div.addEventListener("mouseover", () => {
+			div.classList.remove("gridBlock");
+			div.classList.add("gridBlockOnHover");
+		});
+	});
+}
+
+
+buttonGridSize.addEventListener("click", () => {
+	let gridBlockSize = getUserGridSize();
+
+	deleteGrid();
+	createGrid(gridBlockSize);
+	changeBlockOnHover();
+	// DEBUG
+	console.log("Grid size: " + gridBlockSize);
+});
 
 function initGrid(){
 	setGridSize();
-	createGrid();
-	
-	const block = sketchContainer.querySelectorAll("div");
-
-	return block;
+	createGrid(DEFAULT_GRID_SIZE);
+	changeBlockOnHover();
 }
 
-const blocksHover = initGrid();
-
-blocksHover.forEach((div) => {
-	div.addEventListener("mouseover", () => {
-		div.classList.remove("gridBlock");
-		div.classList.add("gridBlockOnHover");
-	})
-})
+// INITIALIZATION
+initGrid();
