c7c2dd1bf728fd3b6b10119276664963f96186c2
Alex <114047696+bigspaceworm@users.noreply.github.com>
2025-08-27 16:15:55 -0600

    Add toggle between solid and opacity block fill

--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
 const DEFAULT_GRID_SIZE = 16;
 const GRID_SIDE = 35;
+const INIT_OPACITY = 0.1;
 const sketchContainer = document.querySelector("#sketchContainer");
 const buttonGridSize = document.querySelector(".gridSize").querySelector("button");
 const buttonRandomColor = document.querySelector(".randomColor").querySelector("button");
 const buttonSingleColor = document.querySelector(".singleColor").querySelector("button");
 const buttonChangeOpacity = document.querySelector(".changeOpacity").querySelector("button");
+let colorMode = "solid";
 
 function getUserGridSize(){
 	let userGridSize = Number(prompt("Write a number between 2-100 to set the size of the grid", 16));
@@ -28,6 +30,7 @@ function createGrid(gridBlockSize){
 		block.classList.add("gridBlock");
 		block.style.height = blockSide;
 		block.style.width = blockSide;
+		block.style.opacity = INIT_OPACITY;
 
 		sketchContainer.appendChild(block);
 	}
@@ -46,6 +49,10 @@ function changeBlockOnHoverSolid(){
 		div.addEventListener("mouseover", () => {
 			div.style.removeProperty("background");
 			div.removeAttribute("class");
+			let blockOpacity = div.style.opacity;
+			if(blockOpacity != 1){
+				div.style.opacity = setBlockOpacity(blockOpacity);
+			}
 			div.classList.add("gridBlockOnHover");
 		});
 	});
@@ -53,17 +60,29 @@ function changeBlockOnHoverSolid(){
 
 function changeBlockOnHoverRandom(){
 	const blocksHover = sketchContainer.querySelectorAll("div");
-	let randomColor = "white";
+	let randomColor = "";
 
 	blocksHover.forEach((div) => {
 		div.addEventListener("mouseover", () => {
 			randomColor = "#"+((1<<24)*Math.random()|0).toString(16);
 
 			div.removeAttribute("class");
+			let blockOpacity = div.style.opacity;
+			if(blockOpacity != 1){
+				div.style.opacity = setBlockOpacity(blockOpacity);
+			}
 			div.style.background = randomColor;
 			div.classList.add("gridBlockOnHoverRandom");
 		});
 	});
+}
+
+function setBlockOpacity(blockOpacity){
+	if(colorMode === "solid"){
+		return 1;
+	} else {
+		return parseFloat(blockOpacity) + 0.1;
+	}
 }
 
 buttonGridSize.addEventListener("click", () => {
@@ -79,8 +98,22 @@ buttonRandomColor.addEventListener("click", () => {
 });
 
 buttonSingleColor.addEventListener("click", () => {
-	changeBlockOnHoverSolid()
+	changeBlockOnHoverSolid();
 });
+
+buttonChangeOpacity.addEventListener("click",
+() => {
+	const opacityStatusLabel = document.querySelector(".opacityStatus");
+
+	if(colorMode === "solid"){
+		colorMode = "opacity";
+		opacityStatusLabel.textContent = "Opacity: ON";
+	} else {
+		colorMode = "solid";
+		opacityStatusLabel.textContent = "Opacity: OFF";
+	}
+}
+);
 
 function initGrid(){
 	setGridSize();
